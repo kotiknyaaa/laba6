0a1c3bb5175d7a4a4f2e62da1bcc36279fcb8ff0
-kotiknR <kotassasinovic@gmail.com>
2024-11-14 21:17:23 +0300

    Первый прототип (не работает)

--- laba6-1.py
+++ laba6-1.py
@@ -1,4 +1,38 @@
+import random
 while True:
     n=input("Введите длину списка в виде натурального числа:")
     if n.isdigit():
         n=int(n)
+        break
+
+sp=[]
+summa=0
+proizv=0
+pos=" "
+posotric1=" "
+posotric2=" "
+mx=-1
+for _ in range(n):
+    sp.append(random.randint(-10,10))
+print('Список:', sp, sep=" ")
+
+
+for i in range(n):
+    if mx<abs(sp[i]):
+        mx=abs(sp[i])
+        pos=i
+    if sp[i]<0 and posotric1==" ":
+        posotric1=i
+    if sp[i]<0 and posotric1!=" ":
+        posotric2=i
+
+
+proizv=sp[pos+1]
+for j in range(pos,n):
+    proizv=proizv*sp[j]
+
+for k in range(posotric1+1,posotric2):
+    summa+=sp[k]
+
+print("Произведение эл-ов списка после макс. эл-а:", proizv,sep=" ")
+print("Сумма эл-ов списка между отрицательными эл-ами:", summa,sep=" ")
