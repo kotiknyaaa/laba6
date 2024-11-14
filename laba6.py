0e9c0bc1953d54360399df52513405ce76d59802
-kotiknR <kotassasinovic@gmail.com>
2024-11-14 19:42:15 +0300

    test2 xd

--- laba6.py
+++ laba6.py
@@ -1,2 +1,16 @@
-print("hello world")
-print('lslslsls')
+text=input("Введите ваш текст:")
+count=0
+if (")" not in text) and ("(" not in text):
+    print("Скобки отсутствуют!")
+else:
+    for i in text:
+        if i=="(":
+            count+=1
+        if i==")":
+            count-=1
+        if count<0:
+            break
+    if count!=0:
+        print("Скобки в вашем тексте не сбалансированы!")
+    else:
+        print("Скобки сбалансированы! Поздравляем!")
