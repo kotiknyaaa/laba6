895e0177785199eb33dd03a61fa3eb01ce8e2e7c
-kotiknR <kotassasinovic@gmail.com>
2024-11-15 09:53:26 +0300

    Вроде точно зафиксил

--- laba6-1.py
+++ laba6-1.py
@@ -27,17 +27,21 @@ else:
             pos=i
         if sp[i]<0 and posotric1==" ":
             posotric1=i
-        if sp[i]<0:
             posotric2=i
+            if i+1!=n:
+                for l in range(i+1,n):
+                        if sp[l]<0:
+                            posotric2=l
+                            break
 
     if pos+1!=n:
         proizv=sp[pos+1]
         for j in range(pos+2,n):
             proizv=proizv*sp[j]
 
-    for k in range(posotric1+1,posotric2):
-        summa+=sp[k]
-
+    if posotric1!=" ":
+        for k in range(posotric1+1,posotric2):
+            summa+=sp[k]
 
     print("Произведение эл-ов списка после макс. эл-а:", proizv,sep=" ")
     print("Сумма эл-ов списка между отрицательными эл-ами:", summa,sep=" ")
