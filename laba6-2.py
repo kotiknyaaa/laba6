f3c81ff937be95fb1c18d6718fa2980aeb03be0e
-kotiknR <kotassasinovic@gmail.com>
2024-11-21 02:39:42 +0300

    Сделал доп. задание

--- laba6-2.py
+++ laba6-2.py
@@ -1,1 +1,10 @@
-text=input('Введите ваш текст:')
+text=input('Введите ваш текст:').split()
+alpeng="eyuioaEYUIOA"
+alprus="аеёиоуыэюяАЕЁИОУЫЭЮЯ"
+count=0
+
+for i in text:
+    if i[0] in alpeng or i[0] in alprus:
+        count+=1
+
+print("Кол-во слов, начинающихся с гласной:", count, sep=" ")
